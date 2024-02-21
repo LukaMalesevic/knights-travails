da7cba6457997128f3e28b31bb77eab474f8d6c7
LukaMalesevic <lukamalesevic0@gmail.com>
2024-02-21 08:31:15 +0100

    finishing

--- knights-travails.js
+++ knights-travails.js
@@ -1,30 +1,64 @@
-class Board
-{
-    constructor(dimension = 8)
-    {
-        this.dimension = dimension;
-        const board = new Array(8);
+function knightMoves(start, end) {
+    let tree = new Node(start);
+    let queue = [tree];
 
-        for(let i = 0; i < this.dimension; i++)
-        {
-           board[i] = new Array(8).fill(-1);
-        }
+    while (queue.length > 0) {
+        let currentNode = queue.shift();
 
-        this.board = board;
-    }
+        const moves = [
+            [1, 2], [2, 1], [2, -1], [1, -2],
+            [-1, -2], [-2, -1], [-2, 1], [-1, 2]
+        ];
 
-    drawBoard()
-    {
-        for(let i = 0; i < this.board.length; i++)
-        {
-            for(let j = 0; j < this.board.length; j++)
-            {
-                process.stdout.write(String("  " + this.board[i][j]) + "  ");
+        for (let i = 0; i < moves.length; i++) {
+            let newCoordinates = [currentNode.coordinates[0] + moves[i][0], currentNode.coordinates[1] + moves[i][1]];
+            if (checkIfValid(newCoordinates)) {
+                let newNode = new Node(newCoordinates);
+                currentNode.child.push(newNode);
+                queue.push(newNode);
+                if (JSON.stringify(newCoordinates) === JSON.stringify(end)) return [tree, end];
             }
-            console.log("\n");
         }
+    }
+    return null;
+}
+
+class Node
+{
+    constructor(coordinates)
+    {
+        this.coordinates = coordinates;
+        this.child = Array(0);
     }
 }
 
-const board = new Board();
-board.drawBoard();
+function checkIfValid(coordinates)
+{
+    if(coordinates[0] <= 8 && coordinates[1] <= 8 && coordinates[0] >= 0 && coordinates[1] >= 0) return true;
+    else return false;
+
+}
+
+function findEnd(root, end, path = []) {
+    if (!root || !root.child) return null;
+    
+    path.push(root.coordinates);
+    
+    for (let child of root.child) {
+        if (!child) continue;
+        
+        if (child.coordinates[0] === end[0] && child.coordinates[1] === end[1]) {
+            path.push(end);
+            return path;
+        }
+        
+        let result = findEnd(child, end, path.slice());
+        if (result) return result;
+    }
+}
+
+const root = knightMoves([0, 0], [7, 8]); 
+const endPosition = root[1];
+const path = findEnd(root[0], endPosition);
+
+console.log(path);
